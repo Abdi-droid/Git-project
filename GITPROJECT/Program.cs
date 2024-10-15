2a9419cec9f8b920c90a12373ce4570ddf4c048d
abdi awale <loyalabdi98@outlook.com>
2024-10-15 12:13:54 +0100

    initial commit: Skapade enkel kalkylator

---
+++ GITPROJECT/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+
+namespace EnkelKalkylator
+{
+
+    class program
+
+    {
+        static void Main(string[] args) 
+        {
+
+            Console.WriteLine("Enkel kalkylator");
+            Console.WriteLine("Ange operation (+, -m, *, /): ");
+            char operation = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nAnge första talet:");
+            double num1 =  Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ange andra talet:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;   
+                    break;
+                case '*':
+                    result = num1 * num2;   
+                    break;  
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("ogiltig operation");
+                    return;
+
+
+            }
+         
+            Console.WriteLine($"Resultat: {result}");
+            Console.ReadKey();
+                    
+        }
+    }
+}
